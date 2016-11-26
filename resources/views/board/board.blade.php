65bc2ec6db5942df6a18076ec0133c138b2cf010
Simone Stefani <s.stefani95@gmail.com>
2016-11-26 16:16:06 +0200

    Add boardd views

---
+++ resources/views/board/board.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <h3>Board</h3>
+        </div>
+    </div>
+</div>
+@endsection
---
+++ resources/views/board/create.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <h3>Search</h3>
+        </div>
+    </div>
+</div>
+@endsection
