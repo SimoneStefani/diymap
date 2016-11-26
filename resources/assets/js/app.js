a5a7cb1654c020db9db5f58770df798f723ae7b4
Marcel Eschmann <marcel.eschmann@gmail.com>
2016-11-26 15:28:37 +0200

    Remove vue

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,21 +1,14 @@
+function initMap() {
+    var myLatLng = {lat: -25.363, lng: 131.044};
 
-/**
- * First we will load all of this project's JavaScript dependencies which
- * include Vue and Vue Resource. This gives a great starting point for
- * building robust, powerful web applications using Vue and Laravel.
- */
+    var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 4,
+        center: myLatLng
+    });
 
-require('./bootstrap');
-
-/**
- * Next, we will create a fresh Vue application instance and attach it to
- * the page. Then, you may begin adding components to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
- */
-
-Vue.component('example', require('./components/Example.vue'));
-
-const app = new Vue({
-    el: '#app'
-});
-
+    var marker = new google.maps.Marker({
+        position: myLatLng,
+        map: map,
+        title: 'Hello World!'
+    });
+}
