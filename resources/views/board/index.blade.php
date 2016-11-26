db3af708a1638ecbf259f4f707b89c6c36a62922
ekanninen <erkki.kanninen@gmail.com>
2016-11-26 18:34:31 +0200

    Add basic styling, add board card styling and nav

--- resources/views/board/index.blade.php
+++ resources/views/board/index.blade.php
@@ -1,13 +1,17 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
+<div class="container canvas-container">
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
-            <h3>Index page</h3>
+            <div class="canvas-title">Boards</div>
             @foreach($ownedBoards as $owned)
                 <p><a href="{{ url('/boards/'.$owned->id) }}">{{ $owned->title }}</a></p>
             @endforeach
+            <div class="board-card">
+            	<div class="board-card-img"></div>
+            	<div class="board-card-title">Woah what a gathering</div>
+            </div>
         </div>
     </div>
 </div>
