ecfe338b95c869bf208d5f4b73e99e1c9e3b51df
Simone Stefani <s.stefani95@gmail.com>
2016-11-26 11:29:21 +0200

    Add store and destroy board

---
+++ app/Http/Controllers/BoardController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Board;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
+
+class BoardController extends Controller
+{
+    public function store(Request $request)
+    {
+        // If fails, throws 422
+        $this->validate($request, [
+            'title' => 'required|string'
+        ]);
+
+        $newBoard = Auth::user()->ownedBoards->create([
+            'title' => $request->title
+        ]);
+
+        return json_encode($newBoard);
+    }
+
+    public function destroy(string $board)
+    {
+        // If fails, throws 404
+        $board = Board::where('id', $board)->firstOrFail();
+        
+        // If fails, throws 403
+        if (! Auth::check() || ! Auth::user()->id == $board->owner_id) {
+            throw new AccessDeniedHttpException("User not authorised!");
+        }
+
+        // If fails, throws 500
+        $board->delete();
+
+        return json_encode($board);
+    }
+}
