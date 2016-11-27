b901ba9ed19d091559bb009192bc9f8e207bad54
Marcel Eschmann <marcel.eschmann@gmail.com>
2016-11-27 02:53:57 +0200

    Move invite button

--- resources/views/board/board.blade.php
+++ resources/views/board/board.blade.php
@@ -10,14 +10,13 @@
 
 @section('navbar')
 	<div class="navbar-brand pull-right">
-		<button onclick="window.location.href='#'" class="btn btn-nav" type="button">
-		<i class="fa fa-plus" aria-hidden="true"></i>
-		</button>
+        <button id="invite-user" class="btn btn-nav" type="button">
+            <i class="fa fa-user-plus" aria-hidden="true"></i>
+        </button>
 	</div>
 @endsection
 
 @section('content')
-    <a class="btn btn-primary" id="invite-user">Invite</a>
     <div id="map" data-lat="{{ $location->lati }}" data-lng="{{ $location->long }}" data-radius="{{ $location->radius }}"></div>
     <div class="canvas-title canvas-title-inboard">{{ $location->name }}</div>
     <div class="overlay-circle"></div>
